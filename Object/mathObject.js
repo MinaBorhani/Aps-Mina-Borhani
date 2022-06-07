bfb2bc923e759647aaaae33af360d25e56e9fcb6
Mina Borhani <92939258+MinaBorhani@users.noreply.github.com>
2022-06-07 22:51:08 +0430

    commit

--- Object/mathObject.js
+++ Object/mathObject.js
@@ -1,1 +1,12 @@
 console.log(Math);
+
+
+num1 = 8.5;
+num2 = 8.4;
+
+console.log(Math.round(num1));              //9
+console.log(Math.round(num2));              //8
+
+
+console.log(Math.floor(num1));              //9
+console.log(Math.floor(num2));              //8
