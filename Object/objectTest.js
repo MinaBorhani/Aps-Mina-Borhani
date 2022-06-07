fcef06c302dc4dcc1cc1c28ba1a17300aa7ca508
Mina Borhani <92939258+MinaBorhani@users.noreply.github.com>
2022-06-07 19:47:31 +0430

    commit

---
+++ Object/objectTest.js
@@ -0,0 +1,12 @@
+ // syntax Object
+ const user = {
+     name: 'mina',                                                // String
+     age : 23 ,                                                   // number
+     language : ['html', 'css', 'js', 'mongodb'],                 // array
+     login : function() {                                         // function
+         console.log("the user loggedin");
+     },
+     logout () {                                                  // function
+         console.log("the user loggedout");
+     }
+ }
