29a799322874965aff0b6f892cb870040978d921
Mina Borhani <92939258+MinaBorhani@users.noreply.github.com>
2022-06-09 00:51:58 +0430

    commit

--- Object/dom/script.js
+++ Object/dom/script.js
@@ -1,11 +1,17 @@
 // get element by Id , tag , class name
-const getId =  document.querySelector('#page-title');               // get by Id
+// const getId =  document.querySelector('#page-title');               // get by Id
+// console.log(getId);
+
+
+// const getClass = document.querySelector('.content')                 // get by class nae
+// console.log(getClass);
+
+
+// const getTag = document.querySelector('p');                         // get by Tag name
+// console.log(getTag);
+
+
+
+// get element by Id
+const getId = document.getElementById('page-title');
 console.log(getId);
-
-
-const getClass = document.querySelector('.content')                 // get by class nae
-console.log(getClass);
-
-
-const getTag = document.querySelector('p');                         // get by Tag name
-console.log(getTag);
